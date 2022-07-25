d2bcf5818bc58dcb36cd66a311cf1a329230986f
hamidurrk <hamidurrk@gmail.com>
2022-07-25 13:05:16 +0600

    correction

--- pr-2.2.c
+++ pr-2.2.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <math.h>
-
 int i, j, n, start_range, end_range, result, count;
-
 int main() {
     printf("Enter an integer: ");
     scanf("%d", &n);
-    
+
     for (i=1; i<=n; i++) {
         start_range = pow(10, (i-1));
         end_range = pow(10,i) - 1;
+        if (start_range == 1)
+            start_range = 0;
+        else
+            start_range = start_range;
         printf("%d - %d: ", start_range, end_range);
-        for (j = start_range; j < end_range; j++)
+        for (j = start_range; j <= end_range; j++)
         {
             result = isPalindrome(j);
             if (result == 1)
